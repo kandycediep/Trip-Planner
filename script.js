fa94cf870bce9dc47227c5f3292c21de118631d8
kandyce <89169423+kandycediep@users.noreply.github.com>
2021-08-19 00:06:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// open when someone clicks on the span element
+function openNav() {
+  document.getElementById("myNav").style.width = "50%";
+}
+
+// close when someone clicks on the "x" symbol inside the overlay
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+// making automatic slider on homepage 
+// changing image
+var responsiveSlider = function() {
+
+var slider = document.getElementById("slider");
+var sliderWidth = slider.offsetWidth;
+var slideList = document.getElementById("slideWrap");
+var count = 1;
+var items = slideList.querySelectorAll("li").length;
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+
+window.addEventListener('resize', function() {
+  sliderWidth = slider.offsetWidth;
+});
+
+var nextSlide = function() {
+  if(count < items) {
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+  else if(count = items) {
+    slideList.style.left = "0px";
+    count = 1;
+  }
+};
+
+// previous slide button
+var prevSlide = function() {
+  if(count > 1) {
+    count = count - 2;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+  else if(count = 1) {
+    count = items - 1;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+};
+
+// connecting the buttons to respond
+next.addEventListener("click", function() {
+  nextSlide();
+});
+
+prev.addEventListener("click", function() {
+  prevSlide();
+});
+
+setInterval(function() {
+  nextSlide()
+}, 4500); // how fast the transition will be (4.5 seconds)
+
+};
+
+window.onload = function() {
+responsiveSlider();  
+}
